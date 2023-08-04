ace2155cd88447cf0ef7d7a242a750b79b9ef6eb
BilalLiaquat7 <bilal.liaquat7@gmail.com>
2023-08-04 18:59:32 +0500

    API implementation completed

---
+++ src/modules/getId.js
@@ -0,0 +1,16 @@
+export const getGameId = async () => {
+    const url =
+      "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games";
+    const resp = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: "First Game",
+      }),
+    });
+  
+    const game = await resp.json();
+    localStorage.setItem("game_id", game.result.split(" ")[3]);
+  };
