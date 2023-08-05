ccd9f7ca7794c0c8767c38e40f4f634158a696aa
BilalLiaquat7 <bilal.liaquat7@gmail.com>
2023-08-05 14:55:07 +0500

    linters file added and errors are fixed

--- src/modules/get.js
+++ src/modules/get.js
@@ -1,8 +1,8 @@
-export const get = async (endPoint) => {
-  const url =
-    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/" +
-    endPoint;
-  let resp = await fetch(url);
-  let data = resp.json();
+const get = async (endPoint) => {
+  const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${endPoint}`;
+  const resp = await fetch(url);
+  const data = resp.json();
   return data;
 };
+
+module.exports = get();
